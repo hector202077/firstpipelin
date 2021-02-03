e3fd5f843d72c3fae91046278f835a891184bc11
Hector <hectormrg0823@gmail.com>
2021-02-03 06:32:40 -0800

    add logic and test

---
+++ src/main/java/com/demofivetwo/fivetwodemo/FivetwodemoApplication.java
@@ -0,0 +1,13 @@
+package com.demofivetwo.fivetwodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivetwodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivetwodemoApplication.class, args);
+	}
+
+}
